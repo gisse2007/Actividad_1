edac0e90339237c23976abdcd875ad2c2039432a
Saab1919 <blandonsantiago705@gmail.com>
2026-02-20 20:53:19 -0500

    Add Punto3.dart

---
+++ Punto3.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main() {
+  print('Calculadora de puntaje de equipo de futbol');
+  
+  print('Ingrese partidos ganados:');
+  String? input1 = stdin.readLineSync();
+  int ganados = int.parse(input1!);
+  
+  print('Ingrese partidos empatados:');
+  String? input2 = stdin.readLineSync();
+  int empatados = int.parse(input2!);
+  
+  print('Ingrese partidos perdidos:');
+  String? input3 = stdin.readLineSync();
+  int perdidos = int.parse(input3!);
+  
+  int puntosGanados = ganados * 3;
+  int puntosEmpatados = empatados * 1;
+  int puntosPerdidos = perdidos * 0;
+  int totalPuntos = puntosGanados + puntosEmpatados + puntosPerdidos;
+  
+  print('Partidos ganados: $ganados');
+  print('Partidos empatados: $empatados');
+  print('Partidos perdidos: $perdidos');
+  print('Total de puntos: $totalPuntos');
+}
